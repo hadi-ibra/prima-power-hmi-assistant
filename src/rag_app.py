c35e9584c7fe5736935a666e87c9600e47b03c06
Hadi Ibrahim <74880499+hadi-ibra@users.noreply.github.com>
2024-05-28 01:54:03 +0000

    update

--- src/rag_app.py
+++ src/rag_app.py
@@ -35,10 +35,10 @@ if api_key is not None:
     os.environ["LANGCHAIN_API_KEY"] = api_key
 
 
-pickle_file = "/workspaces/prima-power-hmi-assistant/src/data/docs.pkl"
+pickle_file = "data/docs.pkl"
 with open(pickle_file, "rb") as file:
     docs = pickle.load(file)
-dataset = pd.read_csv("/workspaces/prima-power-hmi-assistant/src/data/testset.csv")
+dataset = pd.read_csv("data/testset.csv")
 
 
 if "model" not in st.session_state:
